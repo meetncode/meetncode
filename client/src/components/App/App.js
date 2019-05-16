ba7a9802bc12988e8f3c15f81f23158e9a1da589
Sosodope <sonampenjoredolkar@gmail.com>
2019-05-16 07:55:11 +0700

    Set up folder structure for client
    Install react-redux, redux, material/core and react-router-dom

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 const App = () => (
   <div>
     <h1>MeetnCode</h1>
+    <p>Sonam was here</p>
   </div>
 )
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 
-import App from './App';
+import App from './components/App/App';
 
 const client =  new ApolloClient({
   uri: 'http://localhost:4000/'
