101c6f2f69bb59123253123694782aaed06bf80a
hnin lwin lwin <hnin.yeminymster@gmail.com>
2019-05-24 06:53:15 +0630

    temporary commit

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<div className="terrinary">
+			Company
+			About Us
+			Our Services
+			Resources
+			News & Insight
+			Support
+			Contact Us
+			Resources
+			Twitter
+			</div>
+			<div className="footer-menu">
+			Copyright © 2018 Grada. All rights reserved.
+			Terms & Conditions
+			Privacy Policy
+			</div>
+		</div>
+		)
+}
+
+export default Footer;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+	return (
+		<div className="header-wrapper">
+			<div className="menu-bar-wrapper">
+				<a href="#">start a new group</a>
+				<a href="#">Login</a>
+				<a href="#">register</a>
+			</div>
+		</div>
+	)
+}
+
+export default Header;
