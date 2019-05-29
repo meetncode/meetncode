a7fa117a28750f7575feb75bc75a0dea1b241834
Sosodope <sonampenjoredolkar@gmail.com>
2019-05-29 21:58:19 +0700

    Change class to classname

--- client/src/components/Categories/Category.js
+++ client/src/components/Categories/Category.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Category = () => {
 	return (
 		<li className="category">
-			<i class="fas fa-code-branch"></i>
+			<i className="fas fa-code-branch"></i>
 			<p>ReactJs</p>
 		</li>
 	)
