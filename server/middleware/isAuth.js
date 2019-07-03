c7b2c2d741f3f19dbc995e58ad26d0796a8c3dde
Ivan <ivanms1@gmail.com>
2019-07-03 14:56:16 +0900

    groupId added to getEvents

---
+++ server/middleware/isAuth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+const isAuth = ({ req }) => {
+  const token = req.headers.authorization;
+  if(!token) {
+    return {
+      isAuth: false
+    }
+  }
+
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, 'secretkey');
+  } catch (error) {
+    return {
+      isAuth: false
+    }
+  }
+
+  if(!decodedToken) {
+    return {
+      isAuth: false
+    }
+  }
+
+  return {
+    isAuth: true,
+    userId: decodedToken.userId
+  }
+}
+
+module.exports = isAuth;
