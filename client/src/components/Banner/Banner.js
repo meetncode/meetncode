2570f53978a1e35a43330677ef50c6a9e74400ad
Sosodope <sonampenjoredolkar@gmail.com>
2019-07-06 00:35:51 +0700

    Update material ui version and switch to use material buttons instead of custom btn classes

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -2,15 +2,24 @@ import React from 'react'
 import Background from './background.jpg'
 import './Banner.css'
 import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    marginRight: '1em',
+  }
+}));
 
 const Banner = () => {
+	const classes = useStyles();
 	return (
 		<div style={styles.banner}>
 			<h2 className="headline">Lorem Ipsum Dolor Sit Amet</h2>
 			<p className="tagline">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
 			<div className="call-to-action">
-				<Link to="/" className="btn btn-white">Get Started</Link>
-				<Link to="/" className="btn btn-transparent">Join MeetNCode</Link>
+				<Link to="/" className="btn-link"><Button variant="contained" color="primary" className={classes.button}>Get Started</Button></Link>
+				<Link to="/" className="btn-link"><Button variant="contained" color="primary" className={classes.button}>Join MeetNCode</Button></Link>
 			</div>
 		</div>
 	)
