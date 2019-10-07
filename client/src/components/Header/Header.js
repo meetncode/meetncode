f56314cfd0c7db1bbef65a2c7ce31172527cc563
Sosodope <sonampenjoredolkar@gmail.com>
2019-10-07 23:54:52 +0700

    Check if user logged in and display nav contents

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,44 @@
 import React from 'react'
 import './Header.css'
+import { withRouter } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import mySvg from './logo.svg'
 
-const Header = () => {
+const checkIfUserLoggedIn = () => {
+	let token = localStorage.getItem('token')
+   if(token !== null || token !== '') {
+    return true
+   }
+}
+
+const handleLogOut = (history) => {
+	localStorage.removeItem('token')
+	checkIfUserLoggedIn()
+}
+
+const Header = ({history}) => {
 	return (
 		<div className='header-wrapper'>
 			<img src={mySvg} style={{ 'maxWidth': 40, 'margin': '1em' }}/>
-			<div className='menu-bar-wrapper'>
-				<Link to='/create-group'>start a new group</Link>
-				<Link to='/login'>Login</Link>
-				<Link to='/signup'>register</Link>
-			</div>
+				<div className='menu-bar-wrapper'>
+					{
+						checkIfUserLoggedIn() ?
+						(
+							<React.Fragment>
+								<Link to='/create-group'>Start a new group</Link>
+								<Link to='/' onClick={() => handleLogOut(history)}>Log out</Link>
+							</React.Fragment>
+						) : (
+							<React.Fragment>
+								<Link to='/create-group'>Start a new group</Link>
+								<Link to='/login'>Login</Link>
+								<Link to='/signup'>Register</Link>
+							</React.Fragment>
+						 )
+					}
+				</div>
 		</div>
 	)
 }
 
-export default Header
+export default withRouter(Header)
