1b99d4de8a8930c6077f423f2facb48695c30f45
Sosodope <sonampenjoredolkar@gmail.com>
2019-10-09 23:03:06 +0700

    changes from Ivan's review

--- client/src/components/GroupHeader/GroupHeaderContainer.js
+++ client/src/components/GroupHeader/GroupHeaderContainer.js
@@ -8,34 +8,34 @@ import { Query } from 'react-apollo'
 import GET_GROUP from './queryGetGroup.graphql';
 
 class GroupHeaderContainer extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-				member: true
-		};
+    this.state = {
+        member: true
+    };
 
 }
-	render() {
-		const { id } = this.props.match.params
+  render() {
+    const { id } = this.props.match.params
 
-		return (
-			<Query query={GET_GROUP} variables={{ id }}>
-				{({ data, loading, error }) => {
-					if (loading) return <p>Loading</p>;
-					if (error) return <p>ERROR</p>;
-					const group = data.getGroup
-					return (
-						<div className="group-header-container"> 
-							<GroupHeaderBanner group={group}/>
-							{/*TODO:: check if current user is a member of the group*/}
-							<GroupHeaderMenu member={this.state.member}/>
-						</div>
-				)
-			}}
-		</Query>
-		)
-	}
+    return (
+      <Query query={GET_GROUP} variables={{ id }}>
+        {({ data, loading, error }) => {
+          if (loading) return <p>Loading</p>;
+          if (error) return <p>ERROR</p>;
+          const group = data.getGroup
+          return (
+            <div className="group-header-container"> 
+              <GroupHeaderBanner group={group}/>
+              {/*TODO:: check if current user is a member of the group*/}
+              <GroupHeaderMenu member={this.state.member}/>
+            </div>
+        )
+      }}
+    </Query>
+    )
+  }
 }
 
 export default withRouter(GroupHeaderContainer)
